cc7e2842ee33046eb481c6be5d2a254e87ce1925
Sam <rieszein@gmail.com>
2021-10-08 20:04:39 -0500

    Enlace con la base de datos

--- src/controllers/config.php
+++ src/controllers/config.php
@@ -1,10 +1,27 @@
 <?php
 
-require __DIR__ . '../../../vendor/autoload.php';
+    require __DIR__ . '../../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
-echo $_ENV['EXAMPLE']
+    $link  = mysqli_connect($_ENV['SERVIDOR'] , $_ENV['USER']  , $_ENV['PASSWORD'])
+    or die("Error: No se pudo conectar con el servidor");
+
+    $db = mysqli_select_db( $link, $_ENV['DB'] )
+    or die ( "Error: No se pudo conectar a la Base de datos" );
+
+    $consulta = "SELECT * FROM Modulo";
+
+    $resultado = mysqli_query($link, $consulta) 
+    or die ( "Error: No se pudo realizar la consulta");
+
+    while ($columna = mysqli_fetch_array( $resultado ))
+    {
+        echo "<tr>";
+        echo "<td>" . $columna['idModulo'] . "</td>
+            <td>" . $columna['nombre'] . "</td>";
+        echo "</tr>";
+    }
 
 ?>

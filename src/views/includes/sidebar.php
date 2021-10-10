732b3f9383542bd678dabd29de0f9eb7f565d596
Fernando Monja Vasquez <fernandomonjav@gmail.com>
2021-10-10 12:15:49 -0500

    Agregar componentes head, navbar, sidebar

---
+++ src/views/includes/sidebar.php
@@ -0,0 +1,22 @@
+<div class="app-sidebar p-3 border-end">
+    <a href="#" class="d-flex text-decoration-none mb-4">
+        <h4 class="fs-4 fw-bold">D' Sami Store</h4>
+    </a>
+    <div class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="#" class="nav-link active">Categorías</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link link-dark">Productos</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link link-dark">Pedidos</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link link-dark">Proveedores</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link link-dark">Usuarios</a>
+        </li>
+    </div>
+</div>
